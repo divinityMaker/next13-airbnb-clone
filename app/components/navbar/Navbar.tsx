ed2b58d65fc93469653c7d9c0a5a90e2f6d83186
divinityMaker <truedmaker@gmail.com>
2023-04-08 19:57:28 -0300

    feat: create login functionality

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,12 +1,18 @@
 "use client";
 
+import { User } from "@prisma/client";
+
 import Container from "../Container";
 import Categories from "./Categories";
 import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
 
-const Navbar: React.FC = () => {
+interface INavbar {
+  currentUser?: User | null;
+}
+
+const Navbar: React.FC<INavbar> = ({ currentUser }) => {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div className="py-4 border-b-[1px]">
@@ -14,7 +20,7 @@ const Navbar: React.FC = () => {
           <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
             <Logo />
             <Search />
-            <UserMenu />
+            <UserMenu currentUser={currentUser} />
           </div>
         </Container>
       </div>
